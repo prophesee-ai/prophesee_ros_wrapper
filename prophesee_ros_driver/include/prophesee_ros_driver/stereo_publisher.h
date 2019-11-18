d289aa397f13481ed1c6f1ff63ed88f07060cb6d
Max Elfgen <elfgen@fzi.de>
2019-11-18 19:42:49 +0100

    Copied content from publisher files and changed names

--- prophesee_ros_driver/include/prophesee_ros_driver/stereo_publisher.h
+++ prophesee_ros_driver/include/prophesee_ros_driver/stereo_publisher.h
@@ -1,4 +1,99 @@
-#ifndef STEREO_PUBLISHER_H
-#define STEREO_PUBLISHER_H
+/*******************************************************************
+ * File : prophesee_ros_publisher.h                                *
+ *                                                                 *
+ * Copyright: (c) 2015-2019 Prophesee                              *
+ *******************************************************************/
 
-#endif // STEREO_PUBLISHER_H
+#ifndef STEREO_PUBLISHER_H_
+#define STEREO_ROS_PUBLISHER_H_
+
+#include <sensor_msgs/CameraInfo.h>
+
+#include <prophesee_driver.h>
+
+#include "log_tone_mapper.h"
+
+/// \brief Main class for ROS publisher
+///
+/// Publishes data from Prophesee sensor to ROS topics
+class PropheseeWrapperStereoPublisher {
+public:
+    /// \brief Constructor
+    PropheseeWrapperStereoPublisher();
+
+    /// \brief Destructor
+    ~PropheseeWrapperStereoPublisher();
+
+    /// \brief Starts the camera and starts publishing data
+    void startPublishing();
+
+private:
+
+    /// \brief Opens the camera
+    bool openCamera();
+
+    /// \brief Publishes CD events
+    void publishCDEvents();
+
+    /// \brief Publishes gray-level frames
+    void publishGrayLevels();
+
+    /// \brief Publishes IMU events
+    void publishIMUEvents();
+
+    /// \brief Node handler - the access point to communication with ROS
+    ros::NodeHandle nh_;
+
+    /// \brief Publisher for camera info
+    ros::Publisher pub_info_;
+
+    /// \brief Publisher for CD events
+    ros::Publisher pub_cd_events_;
+
+    /// \brief Publisher for gray-level frame
+    ros::Publisher pub_gl_frame_;
+
+    /// \brief Publisher for IMU events
+    ros::Publisher pub_imu_events_;
+
+    /// \brief Instance of Camera class
+    ///
+    /// Used to access data from a camera
+    Prophesee::Camera camera_;
+
+    /// \brief Instance of LogToneMapper class
+    ///
+    /// Used to reconstract gray-levels from CD and EM data and apply tone mapping
+    LogToneMapper tone_mapper_;
+
+    /// \brief Message for publishing the camera info
+    sensor_msgs::CameraInfo cam_info_msg_;
+
+    /// \brief Path to the file with the camera settings (biases)
+    std::string biases_file_;
+
+    /// \brief Camera name in string format
+    std::string camera_name_;
+
+    /// \brief Camera string time
+    ros::Time start_timestamp_;
+
+    /// \brief Maximum events rate, in kEv/s
+    int max_event_rate_;
+
+    /// \brief Grey-level rate, in fps
+    int graylevel_rate_;
+
+    /// \brief If showing CD events
+    bool publish_cd_;
+
+    /// \brief If showing gray-level frames
+    bool publish_graylevels_;
+
+   /// \brief If showing IMU events
+    bool publish_imu_;
+
+    static constexpr double GRAVITY = 9.81; /** Mean gravity value at Earth surface [m/s^2] **/
+};
+
+#endif /* STEREO_PUBLISHER_H_ */
--- prophesee_ros_driver/src/stereo_publisher.cpp
+++ prophesee_ros_driver/src/stereo_publisher.cpp
@@ -1,25 +1,257 @@
-#include "ros/ros.h"
-#include "std_msgs/String.h"
+/*******************************************************************
+ * File : prophesee_ros_publisher.cpp                              *
+ *                                                                 *
+ * Copyright: (c) 2015-2019 Prophesee                              *
+ *******************************************************************/
 
-int main(int argc, char **argv)
+#include <mutex>
+
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <sensor_msgs/image_encodings.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/Imu.h>
+
+#include <prophesee_event_msgs/Event.h>
+#include <prophesee_event_msgs/EventArray.h>
+
+#include "stereo_publisher.h"
+
+PropheseeWrapperStereoPublisher::PropheseeWrapperStereoPublisher():
+    nh_("~"),
+    biases_file_(""),
+    max_event_rate_(6000),
+    graylevel_rate_(30)
 {
-  ros::init(argc, argv, "stereo_publisher");
-  ros::NodeHandle nh;
+    camera_name_ = "camera";
 
-  ros::Publisher chatter_pub = nh.advertise<std_msgs::String>("chatter", 1000);
+    // Load Parameters
+    nh_.getParam("camera_name", camera_name_);
+    nh_.getParam("publish_cd", publish_cd_);
+    nh_.getParam("publish_graylevels", publish_graylevels_);
+    nh_.getParam("publish_imu", publish_imu_);
+    nh_.getParam("bias_file", biases_file_);
+    nh_.getParam("max_event_rate", max_event_rate_);
+    nh_.getParam("graylevel_frame_rate", graylevel_rate_);
 
-  ros::Rate loop_rate(10);
-  while (ros::ok())
-  {
-    std_msgs::String msg;
-    msg.data = "hello world";
+    const std::string topic_cam_info = "/prophesee/" + camera_name_ + "/camera_info";
+    const std::string topic_cd_event_buffer = "/prophesee/" + camera_name_ + "/cd_events_buffer";
+    const std::string topic_gl_frame = "/prophesee/" + camera_name_ + "/graylevel_image";
+    const std::string topic_imu_sensor = "/prophesee/" + camera_name_ + "/imu";
 
-    chatter_pub.publish(msg);
+    pub_info_ = nh_.advertise<sensor_msgs::CameraInfo>(topic_cam_info, 1);
 
-    ros::spinOnce();
+    if (publish_cd_)
+        pub_cd_events_ = nh_.advertise<prophesee_event_msgs::EventArray>(topic_cd_event_buffer, 500);
 
-    loop_rate.sleep();
-  }
+    if (publish_graylevels_)
+        pub_gl_frame_ = nh_.advertise<cv_bridge::CvImage>(topic_gl_frame, 1);
 
-  return 0;
+    if (publish_imu_)
+        pub_imu_events_ = nh_.advertise<sensor_msgs::Imu>(topic_imu_sensor, 100);
+
+    while (!openCamera()) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        ROS_INFO("Trying to open camera...");
+    }
+
+    // Set the maximum event rate, in kEv/s
+    // Decreasing the max events rate will decrease the latency and also decrease the number of published events
+    // Increasing the max events rate will increase the number of published events and also increase the latency
+    if (!camera_.set_max_event_rate_limit(max_event_rate_)) {
+         ROS_WARN("Failed to set the maximum event rate");
+    }
+
+    // Add camera runtime error callback
+    camera_.add_runtime_error_callback(
+                [](const Prophesee::CameraException &e) { ROS_WARN("%s", e.what()); });
+
+    // Get the sensor config
+    Prophesee::CameraConfiguration config = camera_.get_camera_configuration();
+    auto &geometry = camera_.geometry();
+    ROS_INFO("[CONF] Width:%i, Height:%i", geometry.width(), geometry.height());
+    ROS_INFO("[CONF] Max event rate, in kEv/s: %u", config.max_drop_rate_limit_kEv_s);
+    ROS_INFO("[CONF] Serial number: %s", config.serial_number.c_str());
+
+    // Publish camera info message
+    cam_info_msg_.width = geometry.width();
+    cam_info_msg_.height = geometry.height();
+    cam_info_msg_.header.frame_id = "PropheseeCamera_optical_frame";
+}
+
+PropheseeWrapperStereoPublisher::~PropheseeWrapperStereoPublisher() {
+    camera_.stop();
+
+    nh_.shutdown();
+}
+
+bool PropheseeWrapperStereoPublisher::openCamera() {
+    bool camera_is_opened = false;
+
+    // Initialize the camera instance
+    try {
+        camera_ = Prophesee::Camera::from_first_available();
+        if (!biases_file_.empty()) {
+            ROS_INFO("[CONF] Loading bias file: %s", biases_file_.c_str());
+            camera_.biases().set_from_file(biases_file_);
+        }
+        camera_is_opened = true;
+    } catch (Prophesee::CameraException &e) {
+        ROS_WARN("%s", e.what());
+    }
+    return camera_is_opened;
+}
+
+void PropheseeWrapperStereoPublisher::startPublishing() {
+    camera_.start();
+    start_timestamp_ = ros::Time::now();
+
+    if (publish_cd_)
+        publishCDEvents();
+
+    if (publish_graylevels_)
+        publishGrayLevels();
+
+    if (publish_imu_)
+    {
+        /** We need to enable the IMU sensor **/
+        camera_.imu_sensor().enable();
+
+        /** The class method with the callback **/
+        publishIMUEvents();
+    }
+
+    ros::Rate loop_rate(5);
+    while(ros::ok()) {
+        if (pub_info_.getNumSubscribers() > 0) {
+            cam_info_msg_.header.stamp = ros::Time::now();
+            pub_info_.publish(cam_info_msg_);
+        }
+        loop_rate.sleep();
+    }
+}
+
+void PropheseeWrapperStereoPublisher::publishCDEvents() {
+    // Initialize and publish a buffer of CD events
+    try {
+        Prophesee::CallbackId cd_callback = camera_.cd().add_callback(
+            [this](const Prophesee::EventCD *ev_begin, const Prophesee::EventCD *ev_end) {
+                // Check the number of subscribers to the topic
+                if (pub_cd_events_.getNumSubscribers() <= 0)
+                    return;
+
+                if (ev_begin < ev_end) {
+                    // Define the message for a buffer of CD events
+                    prophesee_event_msgs::EventArray event_buffer_msg;
+                    const unsigned int buffer_size = ev_end - ev_begin;
+                    event_buffer_msg.events.resize(buffer_size);
+
+                    // Header Timestamp of the message
+                    event_buffer_msg.header.stamp.fromNSec(start_timestamp_.toNSec() + (ev_begin->t * 1000.00));
+
+                    // Sensor geometry in header of the message
+                    event_buffer_msg.height = camera_.geometry().height();
+                    event_buffer_msg.width = camera_.geometry().width();
+
+                    // Add events to the message
+                    auto buffer_it = event_buffer_msg.events.begin();
+                    for (const Prophesee::EventCD *it = ev_begin; it != ev_end; ++it, ++buffer_it) {
+                        prophesee_event_msgs::Event &event = *buffer_it;
+                        event.x = it->x;
+                        event.y = it->y;
+                        event.polarity = it->p;
+                        event.ts.fromNSec(start_timestamp_.toNSec() + (it->t * 1000.00));
+                    }
+
+                    // Publish the message
+                    pub_cd_events_.publish(event_buffer_msg);
+
+                    ROS_DEBUG("CD data available, buffer size: %d at time: %llu", buffer_size, ev_begin->t);
+                }
+            });
+    } catch (Prophesee::CameraException &e) {
+        ROS_WARN("%s", e.what());
+        publish_cd_ = false;
+    }
+}
+
+void PropheseeWrapperStereoPublisher::publishGrayLevels() {
+    // Initialize and publish a gray-level frame
+    try {
+        camera_.set_exposure_frame_callback(graylevel_rate_, [this](Prophesee::timestamp t, const cv::Mat &f) {
+            // Check the number of subscribers to the topic
+            if (pub_gl_frame_.getNumSubscribers() <= 0)
+                return;
+
+            // Define the message for the gray level frame
+            cv_bridge::CvImage gl_frame_msg;
+            gl_frame_msg.header.stamp = ros::Time::now();
+            gl_frame_msg.header.frame_id = "PropheseeCamera_optical_frame";
+            gl_frame_msg.encoding = sensor_msgs::image_encodings::TYPE_8UC1;
+            gl_frame_msg.image = tone_mapper_(f);
+
+            // Publish the message
+            pub_gl_frame_.publish(gl_frame_msg);
+
+            ROS_DEBUG("Graylevel data are available");
+        });
+    } catch (Prophesee::CameraException &e) {
+        if (e.code().value() & Prophesee::CameraErrorCode::UnsupportedFeature)
+            publish_graylevels_ = false;
+        else {
+            ROS_WARN("%s", e.what());
+            publish_graylevels_ = false;
+        }
+    }
+}
+
+void PropheseeWrapperStereoPublisher::publishIMUEvents() {
+    // Initialize and publish a buffer of IMU events
+    try {
+        Prophesee::CallbackId imu_callback = camera_.imu().add_callback(
+            [this](const Prophesee::EventIMU *ev_begin, const Prophesee::EventIMU *ev_end) {
+                // Check the number of subscribers to the topic
+                if (pub_imu_events_.getNumSubscribers() <= 0)
+                    return;
+
+                if (ev_begin < ev_end)
+                {
+                    const unsigned int buffer_size = ev_end - ev_begin;
+
+                    for (auto it = ev_begin; it != ev_end; ++it)
+                    {
+                        /** Store data in IMU sensor **/
+                        sensor_msgs::Imu imu_sensor_msg;
+                        imu_sensor_msg.header.stamp.fromNSec(start_timestamp_.toNSec() + (it->t * 1000.00));
+                        imu_sensor_msg.header.frame_id = camera_name_;
+                        imu_sensor_msg.angular_velocity.x = it->gx; //IMU x-axis [rad/s]is pointing left
+                        imu_sensor_msg.angular_velocity.y = it->gy; //IMU y-axis [rad/s]is pointing up
+                        imu_sensor_msg.angular_velocity.z = it->gz; // IMU z-axis[rad/s] is pointing forward
+
+                        imu_sensor_msg.linear_acceleration.x = it->ax * GRAVITY; //IMU x-axis [m/s^2] is pointing left
+                        imu_sensor_msg.linear_acceleration.y = it->ay * GRAVITY; //IMU y-axis [m/s^2] is pointing up
+                        imu_sensor_msg.linear_acceleration.z = it->az * GRAVITY; //IMU z-axis[m/s^2] is pointing forward
+
+                        // Publish the message
+                        pub_imu_events_.publish(imu_sensor_msg);
+                    }
+
+                    ROS_DEBUG("IMU data available, buffer size: %d at time: %llu", buffer_size, ev_begin->t);
+                }
+            });
+    } catch (Prophesee::CameraException &e) {
+        ROS_WARN("%s", e.what());
+        publish_cd_ = false;
+    }
+}
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "prophesee_ros_publisher");
+
+    PropheseeWrapperStereoPublisher wp;
+    wp.startPublishing();
+
+    ros::shutdown();
+
+    return 0;
 }
